6e37d41558a91f7bb3e31e623984b908fb93a6d3
zpliu1126 <39325949+zpliu1126@users.noreply.github.com>
2019-01-23 15:38:54 +0800

    Create footer.html.php

---
+++ includes/footer.html.php
@@ -0,0 +1,10 @@
+
+<footer class="footer ">
+ 		<p class="text-center" style="font-size: 20px; font-style: italic;">	
+ 			Copyright &copy 2018-<span id="footer_year"></span>
+ 			<script>
+ 				var mydate=new Date();
+ 				document.getElementById("footer_year").innerHTML=mydate.getFullYear();
+ 			</script>
+ 		Email:1944532210@qq.com</p>
+ </footer>
