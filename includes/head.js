3c3d6d1c195858507eeddadf99a8a0c58142bc43
zpliu1126 <39325949+zpliu1126@users.noreply.github.com>
2019-02-24 20:11:38 +0800

    Update head.js

--- includes/head.js
+++ includes/head.js
@@ -1,7 +1,7 @@
 /*<meta charset="utf-8">
 <meta name="viewport" content="width=device-width
-init-scale=1.0">
-<link rel="shortcut icon"  href="/image/bitbug_favicon.ico">
+initial-scale=1.0">
+<link rel="shortcut icon"  href="/image/title.ico">
 <!-- jquery在服务器的文件 -->
 <script src="/js/jquery-3.3.1.min.js"></script>
 <!-- bootstrap 的js文件 -->
@@ -12,19 +12,18 @@ init-scale=1.0">
 <script src="/js/printSourceCode.js"></script>*/
 
 var head=["<meta charset=\"utf-8\">",
-"<meta name=\"viewport\" content=\"width=device-width",
-"init-scale=1\">",
-"<link rel=\"shortcut icon\"  href=\"/image/title.ico\">",
+"<meta name=\"viewport\" content=\"width=device-width ",
+"initial-scale=1\">",
+"<link rel=\"shortcut icon\"  href=\"/image/bitbug_favicon.ico\">",
 "<!-- jquery在服务器的文件 -->",
 "<script src=\"/js/jquery-3.3.1.min.js\"></script>",
-"<!-- bootstrap 的js文件 -->",
+"<!-- bootstrap 的js文件 -->",	
 "<script src=\"/js/bootstrap.min.js\"></script>",
 "<!-- bootstrap中的css文件 -->",
 "<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/bootstrap.min.css\">",
 "<!-- 自己写的显示源代码的js文件 -->",
 "<script src=\"/js/printSourceCode.js\"></script>"].join("");
 document.write(head);
-
 /*使用html5->js 代码实现复用*/
 /*https://www.html.cn/tool/html2js/
 */
